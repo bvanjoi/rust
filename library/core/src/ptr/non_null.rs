4f01b05ac0e58583c61878054f32f7bcf9d0215a
Josh Stone <cuviper@gmail.com>
2024-08-26 12:36:58 -0700

    Apply suggestions from code review

--- library/core/src/ptr/non_null.rs
+++ library/core/src/ptr/non_null.rs
@@ -340,7 +340,7 @@ impl<T: ?Sized> NonNull<T> {
     /// # Safety
     ///
     /// When calling this method, you have to ensure that
-    /// the pointer is [convertible to a reference](crate::ptr#pointer-to-reference-conversion)
+    /// the pointer is [convertible to a reference](crate::ptr#pointer-to-reference-conversion).
     ///
     /// # Examples
     ///
@@ -377,7 +377,7 @@ impl<T: ?Sized> NonNull<T> {
     /// # Safety
     ///
     /// When calling this method, you have to ensure that
-    /// the pointer is [convertible to a reference](crate::ptr#pointer-to-reference-conversion)
+    /// the pointer is [convertible to a reference](crate::ptr#pointer-to-reference-conversion).
     /// # Examples
     ///
     /// ```
